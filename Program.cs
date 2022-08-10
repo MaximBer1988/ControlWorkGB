318e4ebb3308c9ad9f68e66f81c610f344a96774
Maxim <_@example.com>
2022-08-10 19:35:08 +0300

    Избавляемся от пустых элементов

--- Program.cs
+++ Program.cs
@@ -13,3 +13,8 @@ for (int i = 0; i < 6; i++)
 }
 Console.Write($"Массив с элементами длиной до 3: [{String.Join(", ", resultarray)}]");
 Console.WriteLine();
+
+var selectresultarray = resultarray.Where(p => p.Length >0); 
+ 
+
+    Console.WriteLine($"Массив без пустых элементов: [{String.Join(", ", selectresultarray)}]");
